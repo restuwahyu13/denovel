50c4146b70ed655567d10a94d219633c9ff7afae
restuwahyu13 <restuwahyu13@gmail.com>
2020-05-21 21:25:32 +0700

    add custom method middleware
    add custom method middleware for render any template engine
    example request using : ctx.render("ejs", "index", {title: "denovel"});

---
+++ vendor/package/engine/index.ts
@@ -0,0 +1,16 @@
+import {renderFileToString} from 'https://deno.land/x/dejs@0.6.0/mod.ts';
+
+export async function viewEngineMiddleware(ctx: any, next: any) : Promise <void> {
+    ctx.render  =  function(engine: string , view: string,  body: object)  {
+        if(typeof engine === "undefined") throw new Error("view engine type is required");
+         if(typeof view === "undefined" && typeof body === "undefined") throw new Error("template and data is required");
+         switch(typeof engine === "string" && typeof view === "string" && typeof  body === "object" && engine) {
+             case "ejs":
+               const ejsTemplate : Promise<string> =  renderFileToString(`${Deno.cwd()}/resources/views/${view}.ejs`, body);
+               return ejsTemplate;
+             default:
+               throw new Error("template must be string");
+         }
+     }
+    await next();
+}
